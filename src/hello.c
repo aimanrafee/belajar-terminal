d96ea88efde95342e676c0e83d1ca364f6dbca33
Muhammad Aiman bin Mohamad Rafee <aimanrafee@users.noreply.github.com>
2026-02-08 17:31:16 +0800

    Chapter 3: Implemented Mini-Shell for CLF Git OS

--- src/hello.c
+++ src/hello.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    printf("Aiman OS: Memulakan sistem...\n");
-    printf("Hello World dari Terminal WSL!\n");
+    char command[50];
+    printf("--- CLF Git OS Kernel Booted ---\n");
+    
+    while(1) {
+        printf("CLF_OS >> ");
+        scanf("%s", command);
+        
+        if(strcmp(command, "exit") == 0) {
+            printf("Shutting down CLF Git OS...\n");
+            break;
+        } else if(strcmp(command, "ver") == 0) {
+            printf("CLF Git OS v1.0 (Classic-Legacy-Future)\n");
+        } else {
+            printf("Error: Command '%s' not found.\n", command);
+        }
+    }
     return 0;
 }
